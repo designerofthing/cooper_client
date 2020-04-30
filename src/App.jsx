25214a2f3c60b9ec7d971f2241fc40f67fb594c8
designerofthing <designerofthings@gmail.com>
2020-04-30 12:11:27 +0200

    creates InputFields component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
 import React, { Component } from "react";
 import DisplayCooperResult from './components/DisplayCooperResult';
+import InputFields from './components/InputFields';
 
 class App extends Component {
   state = {
     distance: "",
     gender: "female",
     age: ""
-  }
+  };
 
   onChangeHandler = e => {
     this.setState({ [e.target.name] : e.target.value })
-  }
+  };
 
   render() {
     return (
       <>
-        <label>Distance</label>
-        <input 
-          name="distance" 
-          id="distance"
-          onChange={this.onChangeHandler}  
-        ></input>
-  
-        <select onChange={this.onChangeHandler} name="gender" id="gender">
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-        </select>
-  
-        <label>Age</label>
-        <input
-          name="age"
-          id="age"
-          onChange={this.onChangeHandler}
-        ></input>
+        <InputFields onChangeHandler={this.onChangeHandler} />
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
