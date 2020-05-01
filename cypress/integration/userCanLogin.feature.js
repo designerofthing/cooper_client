f03c06cb107916d25e308e7c5fb859c8a6362aa9
designerofthing <designerofthings@gmail.com>
2020-05-01 11:08:28 +0200

    adds login json, modifies login test to have mocks, adds user can save data feature test, adds save button

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -1,9 +1,18 @@
 describe('User authenticates', () => {
     beforeEach(() => {
+      cy.server();
       cy.visit('/');
     });
   
     it('successfully with valid credentials', () => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/auth/sign_in",
+        response: "fixture:login.json",
+        headers: {
+          uid: "user@mail.com"
+        }
+      });
       cy.get('#login').click();
       cy.get('#login-form').within(() => {
         cy.get('#email').type('user@mail.com');
@@ -14,6 +23,15 @@ describe('User authenticates', () => {
     });
   
     it('unsuccessfully with invalid credentials', () => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/auth/sign_in",
+        status: "401",
+        response: {
+          errors: ["Invalid login credentials. Please try again."],
+          success: false
+        }
+      });
       cy.get('#login').click();
       cy.get('#login-form').within(() => {
         cy.get('#email').type('user@mail.com');
